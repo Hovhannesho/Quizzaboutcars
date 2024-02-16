a0c5a0b0ab27de52bbd08d6185324e9611384fc5
Java <hovhannes2dallakyan@gmail.com>
2024-02-16 22:19:13 +0400

    Initial commit

---
+++ app/src/main/java/com/example/quizzaboutcars/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.quizzaboutcars;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void main_btn(View view) {
+        switch (view.getId()){
+            case R.id.btn_play:
+                startActivity(new Intent(MainActivity.this , playActivity.class));
+                break;
+            case R.id.btn_setting:
+                startActivity(new Intent(MainActivity.this , settingActivity.class));
+
+                break;
+            case R.id.btn_exit:
+                this.finishAffinity();
+                break;
+        }
+    }
+}
